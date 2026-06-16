e17c5a9ec29c59b58ef3ac8ae6a3bde014e7138a
agentdevan <devanwarner@gmail.com>
2026-06-16 11:49:59 -0400

    Remove stale object stage alias file

--- Sources/Components/ObjectStageSurfaceAliases.swift
+++
@@ -1,7 +0,0 @@
-#if canImport(SwiftUI)
-import SwiftUI
-
-public typealias ObjectStageSurface<Content: View> = AppCard<Content>
-public typealias ObjectStageGlance<Content: View> = WidgetCard<Content>
-public typealias ObjectStageHero<Content: View> = HeroCard<Content>
-#endif
